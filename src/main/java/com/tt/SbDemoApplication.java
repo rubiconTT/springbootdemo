fac25ffc7dc8ea34f1b5950d7bc3653d78d62254
TT <tangjt@dogesoft.cn>
2019-07-08 16:08:27 +0800

    modify main class

--- src/main/java/com/tt/SbDemoApplication.java
+++ src/main/java/com/tt/SbDemoApplication.java
@@ -3,12 +3,18 @@ package com.tt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class SbDemoApplication {
+public class SbDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbDemoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SbDemoApplication.class);
+	}
 }
